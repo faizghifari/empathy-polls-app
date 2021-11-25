c60bed0051e43d0648be277f1803efb6759d0cb5
faizghifari <fgh1997@live.com>
2021-11-25 16:24:08 +0700

    separate vote session read and write, update vote

--- empathy_backend/polls/serializers.py
+++ empathy_backend/polls/serializers.py
@@ -9,20 +9,42 @@ class TimeslotSerializer(serializers.ModelSerializer):
 
 
 class VoteSerializer(serializers.ModelSerializer):
+    preferences = serializers.SerializerMethodField()
+
     class Meta:
         model = Vote
         fields = "__all__"
-        extra_kwargs = {
-            "notes": {"required": False},
-        }
+        extra_kwargs = {"notes": {"required": False}}
+
+    def get_preferences(self, obj):
+        return obj.get_preferences_display()
+
+    def validate(self, data):
+        voted_ts = Timeslot.objects.get(pk=data["timeslot_id"].id)
+        if data["session_id"].uuid != voted_ts.session_id.uuid:
+            raise serializers.ValidationError(
+                "session_id between vote and timeslot must be the same"
+            )
+        return data
 
 
-class VoteSessionSerializer(serializers.ModelSerializer):
+class VoteSessionWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = VoteSession
-        fields = "__all__"
+        fields = ["uuid", "title", "place", "is_mandatory", "open_public", "notes"]
         extra_kwargs = {
             "title": {"required": True},
             "place": {"required": False},
             "notes": {"required": False},
         }
+
+
+class VoteSessionReadSerializer(serializers.ModelSerializer):
+    timeslots = serializers.SlugRelatedField(
+        many=True, read_only=True, slug_field="datetime"
+    )
+    votes = VoteSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = VoteSession
+        fields = "__all__"
