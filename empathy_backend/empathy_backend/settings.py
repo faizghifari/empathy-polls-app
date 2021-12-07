c9915f2f4669de2d9e99ccea0f905041ead36094
faizghifari <fgh1997@live.com>
2021-12-07 21:44:09 +0700

    change cors origin allow settings

--- empathy_backend/empathy_backend/settings.py
+++ empathy_backend/empathy_backend/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = False
 
 # Django Heroku
